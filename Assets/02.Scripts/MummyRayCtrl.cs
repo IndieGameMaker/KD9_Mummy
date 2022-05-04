25dd1de09a4a8bd03800593109fd14612c43d29e
Lee JaeHyun <myleje@gmail.com>
2022-05-04 10:32:04 +0900

    OnEpisodeBegin 수정

--- Assets/02.Scripts/MummyRayCtrl.cs
+++ Assets/02.Scripts/MummyRayCtrl.cs
@@ -7,17 +7,33 @@ using Unity.MLAgents.Actuators;
 
 public class MummyRayCtrl : Agent
 {
+    private Transform tr;
+    private Rigidbody rb;
+
     private StageManager stageManager;
 
     public override void Initialize()
     {
-        MaxStep = 100;
+        MaxStep = 5000;
+
+        tr = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+
         stageManager = transform.root.GetComponent<StageManager>();
     }
 
     public override void OnEpisodeBegin()
     {
         stageManager.InitStage();
+
+        // 물리엔진의 초기화
+        rb.velocity = rb.angularVelocity = Vector3.zero;
+        // 에이전트의 위치 변경
+        tr.localPosition = new Vector3(Random.Range(-20.0f, 20.0f),
+                                       0.05f,
+                                       Random.Range(-20.0f, 20.0f));
+        // 에이전트의 회전값 변경
+        tr.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
     }
 
     public override void CollectObservations(VectorSensor sensor)
